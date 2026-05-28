42b850ed6cfbf0742cc02bdbb3e2d83fe9e31b8e
Mulei Mutuku <117795291+sha-ddie@users.noreply.github.com>
2026-05-28 10:14:58 +0300

    Updated KPI diplays

--- app.py
+++ app.py
@@ -648,20 +648,20 @@ def render_ro_page(name,df,arrears_agg,dis_tat):
 
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.markdown("#### 👤Customers")
+        st.markdown("##### 👤Customers")
         st.markdown(f"#### {customers_count:,.0f}")
     with col2:
-        st.markdown("#### 💼Portfolio")
+        st.markdown("##### 💼Portfolio")
         st.markdown(f"#### {total_portfolio:,.0f}")
     with col3:
-        st.markdown("#### 💰Disbursment")
+        st.markdown("##### 💰Disbursment")
         st.markdown(f"#### {total_disbursments:,.0f}")
     with col4:
-        st.markdown("#### ⚠️ Arrears")
+        st.markdown("##### ⚠️ Arrears")
         st.markdown(f"#### {total_arrears:,.0f}")
         st.caption(f"Arrear {total_arrears/total_portfolio:.1%} of loan book")
     with col5:
-        st.markdown("#### 📉 PAR")
+        st.markdown("##### 📉 PAR")
         st.markdown(f"#### {par:.2%}")
         st.caption(f"Non-Performing loan book: {non_performing/1_000_000:,.2f}M ")
 
